892f2b6fd64d99e7b73f925cb41f59b0555626c1
Rinita Nair <rnair3@uncc.edu>
2019-09-05 13:48:30 -0400

    Tile system and player scripts

---
+++ Tile Mania/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    //Config
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    //state
+    bool isAlive = true;
+    //Cache
+    Rigidbody2D rigidbody;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+        FlipSide();
+    }
+
+    public void Run()
+    {
+        float controlFlow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 velocity = new Vector2(controlFlow*speed, rigidbody.velocity.y);
+        rigidbody.velocity = velocity;
+
+        bool playerHasHorizontalVelocity = Mathf.Abs(rigidbody.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isWalking", playerHasHorizontalVelocity);
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+            rigidbody.velocity += jumpVelocity;
+        }
+    }
+
+    private void FlipSide()
+    {
+        bool playerHasHorizontalVelocity = Mathf.Abs(rigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalVelocity)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidbody.velocity.x), 1f);
+        }
+    }
+}
